4c89f3c408e94123194fa2118c7aafe78f6399d5
Huakas <44316385+Huakas@users.noreply.github.com>
2018-10-20 21:31:00 +0200

    App Layout

--- app/src/main/java/io/github/jugendhackt/fishtest/MainActivity.java
+++ app/src/main/java/io/github/jugendhackt/fishtest/MainActivity.java
@@ -3,6 +3,8 @@ package io.github.jugendhackt.fishtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
+        //TextView textView = findViewById(R.id.textView);          //Wenn Button gedrückt,
+        //textView.setVisibility(TextView.VISIBLE);                 //zeige diesen Text
+        //TextView textView = findViewById(R.id.textView);          //Wenn Scan abgeschlossen,
+        //textView.setVisibility(TextView.INVISIBLE);               //zeige diesen Text
         //ImageView imageview = findViewById(R.id.imageView);       //Zum Überschreiben wenn
-        //imageview.setImageResource(R.drawable.ic_wanzepicred);  //ein Echo gefunden ist
+        //imageview.setImageResource(R.drawable.ic_wanzepicred);    //ein Echo gefunden ist
     }
 }
