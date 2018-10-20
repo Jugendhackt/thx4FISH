fb23d41d6184931ef4d6ad4055604e0b1184120d
jer <julian.ribbeck@gmx.de>
2018-10-20 16:16:24 +0200

    Reworked the files

---
+++ app/src/main/java/io/github/jugendhackt/fishtest/MainActivity.java
@@ -0,0 +1,16 @@
+package io.github.jugendhackt.fishtest;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+        //ImageView imageview = findViewById(R.id.imageView);       //Zum Überschreiben wenn
+        //imageview.setImageResource(R.drawable.ic_wanzepicgreen);  //ein Echo gefunden ist
+    }
+}
